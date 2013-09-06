4ff149fcb79c229b8305d397011aa14d6fdf55e9
Blake Eggleston <bdeggleston@gmail.com>
2013-09-06 15:53:11 -0700

    adding basic client server

--- punisher/cluster/ClientServer.py
+++ punisher/cluster/ClientServer.py
@@ -1,9 +1,22 @@
+from gevent.server import StreamServer
+
 from punisher.cluster.Cluster import Cluster
+from punisher.cluster.Connection import Connection
 
 
-class RedisClientServer(object):
+class RedisClientServer(StreamServer):
 
-    def __init__(self, cluster, address=('', 6379)):
-        super(RedisClientServer, self).__init__()
+    def __init__(self, listener, cluster, backlog=None, spawn='default', **ssl_args):
+        listener = listener or ('', 4379)
+        super(RedisClientServer, self).__init__(listener, self.handle, backlog, spawn, **ssl_args)
         assert isinstance(cluster, Cluster)
-        self.address = address
+        self.cluster = cluster
+
+    def handle(self, socket, address):
+        """
+        entry point for new connections
+
+        :param socket:
+        :param address:
+        """
+        conn = Connection(socket)
