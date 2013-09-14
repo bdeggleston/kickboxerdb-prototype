63c82864bbdd32facd0880ed0adcb58d6637c88f
Blake Eggleston <bdeggleston@gmail.com>
2013-09-13 19:16:07 -0700

    adding test for key routing

--- punisher/cluster/tests/test_key_routing.py
+++ punisher/cluster/tests/test_key_routing.py
@@ -1,1 +1,33 @@
-__author__ = 'bdeggleston'
+import gevent
+
+from punisher.partitioner.base import BasePartitioner
+from punisher.tests.base import BaseNodeTestCase
+
+
+class LiteralPartitioner(BasePartitioner):
+    """
+    returns the number passed into the key,
+    keys can only be stringified ints
+    """
+    max_token = 10000
+
+    @classmethod
+    def get_key_token(cls, key):
+        return int(key)
+
+
+class KeyRoutingTest(BaseNodeTestCase):
+
+    def test_key_routing(self):
+        num_nodes = 10
+        tokens = [1000 * i for i in range(num_nodes)]
+        self.create_nodes(num_nodes, tokens=tokens, partitioner=LiteralPartitioner())
+
+        for node in self.nodes:
+            node.start()
+        gevent.sleep(0)
+
+        # sanity check
+        for i in range(10):
+            self.assertEqual(self.nodes[i].token, 1000 * i)
+
