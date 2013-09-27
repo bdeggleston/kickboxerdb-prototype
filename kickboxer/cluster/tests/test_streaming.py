ae05cffe686cc682a54955e108c9aae05aaa3616
Blake Eggleston <bdeggleston@gmail.com>
2013-09-26 21:02:52 -0700

    adding stubbed tests for queries while streaming

--- kickboxer/cluster/tests/test_streaming.py
+++ kickboxer/cluster/tests/test_streaming.py
@@ -1,8 +1,30 @@
 from kickboxer.tests.base import BaseNodeTestCase
 
 
-class StreamingQueryTest(BaseNodeTestCase):
+class BaseStreamingQueryTest(BaseNodeTestCase):
     """
     tests querying nodes that are receiving streaming data
     forward queries to the streaming node
     """
+
+
+class JoiningNodeQueryTest(BaseStreamingQueryTest):
+    """
+    when a node joins, and is streaming data from the
+    node to it's left, it should forward all read queries
+    to the streaming node
+    """
+    pass
+
+
+class TokenChangeQueryTest(BaseStreamingQueryTest):
+    """
+    when a node changes it's token, and is streaming data
+    from the node to it's left, it should forward all read
+    queries to the streaming node
+    """
+    pass
+
+
+class NodeRemovalQueryTest(BaseStreamingQueryTest):
+    pass
